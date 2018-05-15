54a65bb3fdac1db322d5329482427668c007a56f
Fernando Fragoso <fernandofragoso@gmail.com>
2018-05-15 16:43:15 -0300

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Route exact={true} path="/" render={() => (
+            <Link to="/another">
+              <button className="btn btn-primary">
+                Go to another route
+              </button>
+            </Link>
+          )}/>
+          <Route path="/another" render={() => (
+            <Link to="/">
+              <button className="btn btn-primary">Go to index</button>
+            </Link>
+          )}/>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Router>
     );
   }
 }
